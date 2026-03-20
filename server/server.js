803acd9043f1be7e7401a4dbbfe87e5b257d2893
rajveer <en22cs304051@medicaps.ac.in>
2026-03-20 16:23:33 +0530

    Describe what you changed

--- server/server.js
+++ server/server.js
@@ -19,8 +19,9 @@ connectDB();
 
 // middleware
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: "https://your-vercel-app.vercel.app"
+}));
 // routes
 app.use("/api/auth", authRoutes);
 
